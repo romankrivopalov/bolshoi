eeb794e253d5d9ea95dd1524a0bb6f3c9380b194
Work desktop <the.red.lemon@yandex.ru>
2022-11-14 16:26:43 +0300

    procces slider in header block

--- js/script.js
+++ js/script.js
@@ -12,3 +12,55 @@ hamburger.addEventListener('click', () => {
 closeSidepanel.addEventListener('click', () => {
   sidepanel.classList.remove('sidepanel_active');
 });
+
+// slider
+let slideIndex = 1,
+    offset = 0;
+
+const slides = document.querySelectorAll('.slider__img'),
+      btnPrev = document.querySelector('#btn-prev'),
+      btnNext = document.querySelector('#btn-next'),
+      slidesWrapper = document.querySelector('.slider__wrapper'),
+      slidesField = document.querySelector('.slider__items'),
+      slideItem = document.querySelector('.slider__item'),
+      widthSlide = 450;
+
+slidesField.style.width = widthSlide * slides.length + 'px';
+slidesField.style.transition = '0.8s all ease-in-out';
+
+slides.forEach(slide => {
+  let imgSlide = slide.src;
+  slideItem.style.backgroundImage = imgSlide;
+})
+
+btnNext.addEventListener('click', () => {
+  if (offset == widthSlide * (slides.length - 1)) {
+    offset = 0;
+  } else {
+    offset += widthSlide;
+  }
+
+  slidesField.style.transform = `translateX(-${offset}px)`
+
+  if (slideIndex == slides.length) {
+    slideIndex = 1;
+  } else {
+    slideIndex++;
+  }
+});
+
+btnPrev.addEventListener('click', () => {
+  if (offset == 0) {
+    offset = widthSlide * (slides.length - 1);
+  } else {
+    offset -= widthSlide;
+  }
+
+  slidesField.style.transform = `translateX(-${offset}px)`;
+
+  if (slideIndex == 1) {
+    slideIndex = slides.length;
+  } else {
+    slideIndex--;
+  }
+});
